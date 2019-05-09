056170a82c7fb7691351eeb0b6a3a76cc895941a
{smarshy} <{vatsala.swaroop@gmail.com}>
2019-05-09 21:45:30 +0000

    colab -> github

--- colorbot/index.js
+++ colorbot/index.js
@@ -132,22 +132,18 @@ class Classifier {
     }
     const input = inputBuffer.toTensor();
     //console.log(input);
+    console.log(input)
 
     status('Running inference');
     const beginMs = performance.now();
     const predictOut = this.model.predict(input);
-    const predictOut2 = this.model.predict("light blue");
     //console.log(predictOut.dataSync());
     console.log("predicted value");
     //console.log(predictOut)
     const score_1 = predictOut.dataSync();//[0];
     console.log(score_1)
     console.log("score should be printed out")
-    const score_2 = predictOut2.dataSync();//[0];
-    console.log(score_2)
-    console.log("score 2 should be printed out")
     predictOut.dispose();
-    predictOut2.dispose();
     const endMs = performance.now();
 
     return {score: score, elapsed: (endMs - beginMs)};
