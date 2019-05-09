6240a419fe9573abfefe5e1d0c33875a655aa011
{smarshy} <{vatsala.swaroop@gmail.com}>
2019-05-09 22:49:07 +0000

    colab -> github

--- colorbot/index.js
+++ colorbot/index.js
@@ -127,8 +127,8 @@ class Classifier {
     const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
     for (let i = 0; i < inputText.length; ++i) {
       const word = inputText[i];
-      inputBuffer.set(this.wordIndex[word], 0, this.maxLen - inputText.length + i);
-      console.log(word, this.wordIndex[word], inputBuffer);
+      inputBuffer.set(this.wordIndex[word], 0, i);
+      //console.log(word, this.wordIndex[word], inputBuffer);
     }
     const input = inputBuffer.toTensor();
     //console.log(input);
